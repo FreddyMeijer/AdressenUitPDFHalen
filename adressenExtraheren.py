38cf7872767a097556f06bc0f508361a0eeae82c
= <=>
2023-10-10 08:45:24 +0200

    producttype 4 toegevoegd

--- adressenExtraheren.py
+++ adressenExtraheren.py
@@ -13,9 +13,9 @@ def selectFile():
 def adressenExtraheren(bestand):
 
     while True:
-        product = input("Typ het producttype (alleen 02 op dit moment)") #04, 08, 09, 21, 22, 30 of 51): ")
+        product = input("Typ het producttype (alleen 02 en 04 op dit moment): ") #04, 08, 09, 21, 22, 30 of 51): ")
         try:
-            if product in ["02"]: #,"04","08","09","21","22","30","51"]:
+            if product in ["02","04"]: #,"04","08","09","21","22","30","51"]:
                 break
         except ValueError:
             print("Onjuist product gekozen.")
@@ -34,13 +34,32 @@ def adressenExtraheren(bestand):
     lines = extracted_text.splitlines()
 
     if product == "02":
-        with open("output.csv", "w", encoding="utf-8") as file:
+        with open("adressen_producttype_02.csv", "w", encoding="utf-8") as file:
             file.write("KENTEKEN;NAAM;ADRES;POSTCODE\n")
             for i in range(len(lines)):
                 if "Retouradres: Postbus 495, 2300 AL Leiden" in lines[i]:
                     uniekkenmerk = lines[i + 30]
                     uniekkenmerk = uniekkenmerk[17:].split(" ", 1)
                     uniekkenmerk = uniekkenmerk[0]
+                    adres = (
+                        uniekkenmerk
+                        + ";"
+                        + lines[i + 1]
+                        + ";"
+                        + lines[i + 2]
+                        + ";"
+                        + lines[i + 3]
+                    )
+                    adres = adres[:-27] + "\n"
+                    file.write(adres)
+    
+    if product == "04":
+        with open("adressen_producttype_04.csv", "w", encoding="utf-8") as file:
+            file.write("AANSLAGBILJETNUMMER;NAAM;ADRES;POSTCODE\n")
+            for i in range(len(lines)):
+                if "Retouradres: Postbus 495, 2300 AL Leiden" in lines[i]:
+                    uniekkenmerk = lines[i + 15]
+                    uniekkenmerk = uniekkenmerk[29:]
                     adres = (
                         uniekkenmerk
                         + ";"
